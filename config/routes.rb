3dd42af3625874f435162e41b4257caf5666b325
kiyoshi.chieda <kiyoshi0907@icloud.com>
2020-06-03 12:46:12 +0000

    favorites

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,13 @@ Rails.application.routes.draw do
     member do
       get :followings
       get :followers
+      get :likes
     end
   end
   
+  get 'likes', to: 'users#likes'
+  
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
